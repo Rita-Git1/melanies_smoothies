3eaefdccabf79380261772462a084d57f30e061f
Rita-Git1 <rita.v.gandham@kipi.ai>
2024-10-15 11:32:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ if ingredients_list:
 time_to_insert = st.button("Submit Order")
 if time_to_insert:
   session.sql(my_insert_stmt).collect()
-  st.success('Your Smoothie is ordered MellyMe!', icon="✅")
+  st.success('Your Smoothie is ordered MellyMe!')
